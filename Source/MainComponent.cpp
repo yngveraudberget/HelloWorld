ebf0e336a4adf2b473a319e96aab92a16bbdd44f
Yngve Raudberget <yngve@yngweb.com>
2020-03-19 23:48:50 +0100

    Changed ui
    Changed ui2

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -24,8 +24,8 @@ void MainComponent::paint (Graphics& g)
     // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
 
-    g.setFont (Font (16.0f));
-    g.setColour (Colours::white);
+    g.setFont (Font (18.0f));
+    g.setColour (Colours::red);
     g.drawText ("Hello World!", getLocalBounds(), Justification::centred, true);
 }
 
